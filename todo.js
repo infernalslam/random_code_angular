e46b235b500d1990e2d7f1af0e8934762d0cffc2
TAK <infernal-slam@hotmail.com>
2016-01-26 18:46:39 +0700

    success algorithm

--- todo.js
+++ todo.js
@@ -3,10 +3,31 @@ angular.module('todoApp', [])
     var todoList = this;
     todoList.title = "Random"
 
+    var nums = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17]
+    var ranNums = []
+    var i = nums.length
+    var j = 0
 
 
     todoList.Random = function () {
-       var number = Math.floor((Math.random() * 17) + 1);
-       console.log(number)
-    }
+      console.log("function in")
+      while (i--) {
+          j = Math.floor(Math.random() * (i+1));
+          ranNums.push(nums[j]);
+          nums.splice(j,1);
+        }
+
+        for (var k = 0; k < ranNums.length; k++) {
+        console.log (ranNums[k])
+      }
+    } //end function
+
+
+
+
+
+
+
+
+
   }) /*end program*/
